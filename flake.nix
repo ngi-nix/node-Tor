05b9d0066b82bc91a54923826bb9260ba3c933ba
Martin Toman <toman.martin@live.com>
2022-07-21 08:16:37 -0700

    Fetch src as flake input

--- flake.nix
+++ flake.nix
@@ -1,7 +1,13 @@
 {
-  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    node-Tor-src = {
+      url = "github:Ayms/node-Tor/master";
+      flake = false;
+    };
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, node-Tor-src }:
     let
       supportedSystems =
         [ "x86_64-linux" "x86_64-darwin" "i686-linux" "aarch64-linux" ];
@@ -10,7 +16,43 @@
 
     in {
       overlays.default = final: prev: {
-        node-Tor = final.callPackage ./default.nix { };
+        node-Tor = with final; stdenv.mkDerivation rec {
+          pname = "node-Tor";
+          version = "0.0.0";
+
+          src = node-Tor-src;
+          buildInputs = [
+            nodePackages.browserify
+            nodePackages.terser
+          ];
+
+          buildPhase = ''
+            cd lib
+            browserify node-tor.js > ../html/browser.js
+            cd ../html
+            terser browser.js -c -m -o minified.js
+            cd ..
+          '';
+          installPhase = ''
+            # lib/node
+            mkdir -p $out/lib/node
+            cp -r lib/* $out/lib/node
+            mv $out/lib/node/node-tor.js $out/lib/node/node-Tor.js
+            cat > $out/lib/node/package.json << EOF
+            {
+              "name": "${pname}",
+              "version": "${version}",
+              "license": "MIT",
+              "main": "node-Tor.js"
+            }
+            EOF
+
+            # lib/browser
+            mkdir -p $out/lib/browser
+            cp html/browser.js $out/lib/browser/node-Tor.js
+            cp html/minified.js $out/lib/browser/node-Tor.min.js
+          '';
+        };
       };
 
       defaultPackage = forAllSystems (system:
